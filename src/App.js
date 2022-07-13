6bc967a4531ce82493d16d933fa095e29fcb91b7
Your Name <you@example.com>
2022-07-12 23:46:06 -0300

    subindo arquivos do mapbox

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Maps from './components/Maps';
+import React from 'react';
 
 function App() {
   return (
--- src/components/Maps/index.js
+++ src/components/Maps/index.js
@@ -1,21 +1,42 @@
-import React, {useState} from 'react';
-import Map from 'react-map-gl';
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import './style.css'; 
 
-const Maps = () => {
 
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3RvcmVhc3kiLCJhIjoiY2w1YjNoeWQxMDN0cDNjbXQxZXBmNTE3diJ9.RrfFrNGcJHNlonfI09pXNA';
  
-
-  return (
-    <Map 
-    initialViewState={{
-      latitude: -25.665865,
-      longitude: -49.3421069,
-      zoom: 10 
-    }}
-    style={{width: 600, height: 400}}
-    mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
-  )
+export default function App() {
+const mapContainer = useRef(null);
+const map = useRef(null);
+const [lng, setLng] = useState(-70.9);
+const [lat, setLat] = useState(42.35);
+const [zoom, setZoom] = useState(9);
+ 
+useEffect(() => {
+if (map.current) return; // initialize map only once
+map.current = new mapboxgl.Map({
+container: mapContainer.current,
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [lng, lat],
+zoom: zoom
+});
+});
+ 
+useEffect(() => {
+if (!map.current) return; // wait for map to initialize
+map.current.on('move', () => {
+setLng(map.current.getCenter().lng.toFixed(4));
+setLat(map.current.getCenter().lat.toFixed(4));
+setZoom(map.current.getZoom().toFixed(2));
+});
+});
+ 
+return (
+<div>
+<div className="sidebar">
+Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+</div>
+<div ref={mapContainer} className="map-container" />
+</div>
+);
 }
-
-export default Maps
